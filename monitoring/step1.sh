e9f32e15a1ee040856cfd43fbb8b75bd205f149e
JBerny <f.bernardi89@gmail.com>
2020-01-06 16:02:31 +0100

    changed wait loop

--- monitoring/step1.sh
+++ monitoring/step1.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-# configure kubectl
+
+check_k8s_ready() {
+  return kubectl get nodes --no-headers | grep "Ready"
+}
+
 echo "Copying kubernetes config"
 cp /etc/kubernetes/admin.conf .kube/config
 
 echo "Waiting for kubernetes to start"
 i=0
-while [ kubectl get nodes --no-headers | grep Ready ] || [ $i -lt 10 ]; do
+while [ $i -lt 10 ]; do
   i=$((i+1))
+  echo "Attempt nr $i"
+  [ check_k8s_ready ] && break
   sleep 2
 done
 
