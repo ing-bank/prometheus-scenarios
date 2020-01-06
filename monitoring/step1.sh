b047e11ff8b1a18a1faa2b925811ee83f2640fc2
JBerny <f.bernardi89@gmail.com>
2020-01-06 15:54:21 +0100

    added wait before deploy

--- monitoring/step1.sh
+++ monitoring/step1.sh
@@ -3,6 +3,13 @@
 echo "Copying kubernetes config"
 cp /etc/kubernetes/admin.conf .kube/config
 
+echo "Waiting for kubernetes to start"
+i=0
+while [ kubectl get nodes --no-headers | grep Ready ] || [ $i -lt 10 ]; do
+  i=$((i+1))
+  sleep 2
+done
+
 # deploy apps
 echo "Deploying apps"
 kubectl -f deployment.yml
