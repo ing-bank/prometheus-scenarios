1ea0b17cc90d96f7045e71522d5d1aba2af95b15
jberny <f.bernardi89@gmail.com>
2020-03-28 12:42:16 +0100

    added docker setup

--- monitoring/env-init.sh
+++ monitoring/env-init.sh
@@ -1,23 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Wait for katacoda"
-sleep 20
+sleep 3
 
-mkdir .kube
-while [ ! -f /etc/kubernetes/admin.conf ]; do 
-  echo "Kubernetes is being installed..."
-  sleep 2; 
-done
-cp /etc/kubernetes/admin.conf .kube/config
-
-i=0
-while [ $i -lt 30 ]; do
-  echo "Waiting for kubernetes to start..."
-  i=$((i+1))
-  (kubectl get nodes --no-headers 2>/dev/null | grep -v "NotReady" | grep "Ready" ) && break
-  sleep 3
-done
-history -c
-# deploy apps
-echo "Deploying apps"
-kubectl apply -f /tmp/deployment.yml
+echo "Deploying apps..."
+docker compose up
