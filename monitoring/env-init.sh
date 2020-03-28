6958056c4fa9d8dc589b9f52104563f57d26173a
jberny <f.bernardi89@gmail.com>
2020-03-28 09:48:39 +0100

    increased waiting time and copied kubectl config

--- monitoring/env-init.sh
+++ monitoring/env-init.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-echo "Wait 3 seconds for katacoda"
-sleep 3
+echo "Wait for katacoda"
+sleep 10
 
-KCTL="kubectl --kubeconfig=/etc/kubernetes/admin.conf"
-
+mkdir .kube
+cp /etc/kubernetes/admin.conf .kube/config
 
 echo "Waiting for kubernetes to start"
 i=0
-while [ $i -lt 10 ]; do
+while [ $i -lt 30 ]; do
   i=$((i+1))
   echo "Attempt nr $i"
-  ($KCTL get nodes --no-headers | grep "Ready") && break
+  (kubectl get nodes --no-headers | grep "Ready") && break
   sleep 2
 done
 
 # deploy apps
 echo "Deploying apps"
-$KCTL apply -f /tmp/deployment.yml
+kubectl apply -f /tmp/deployment.yml
