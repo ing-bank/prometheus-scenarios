ba2d29a208bf123d28b20521b9f0a1851da17986
JBerny <f.bernardi89@gmail.com>
2020-05-01 13:15:12 +0200

    refactored to use custom images instead of volumes

--- monitoring/env-init.sh
+++ monitoring/env-init.sh
@@ -12,10 +12,11 @@ echo "Wait for katacoda"
 sleep 3
 
 echo "Deploying apps..."
-# docker-compose -f monitoring-apps.yml up -d || panic """
-# Error while setting up the environment.
-# Try to refresh the window.
-# """
+cd /tmp/assets
+docker-compose -f monitoring-apps.yml up -d || panic """
+Error while setting up the environment.
+Try to refresh the window.
+"""
 
 echo "Everything done, you can start learning!"
 
