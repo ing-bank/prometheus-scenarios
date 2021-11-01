42f090f1c1c51e82d7a6c778383b4d403d39888a
Ernst Teuling <ernst.teuling@ing.com>
2021-11-01 06:40:26 +0100

    Added a check that docker is running to the start script for Linux/Mac

--- promql/start-learning-environment.sh
+++ promql/start-learning-environment.sh
@@ -4,5 +4,10 @@
 #
 cd "$(dirname $0)"
 export PROMQLWD="$(pwd)"
+docker ps > /dev/null 2>&1
+if test $? -ne 0; then
+  echo "Docker does not seem to be running, or you do not have access to use it"
+  exit 1
+fi
 docker-compose up
 # end of script
